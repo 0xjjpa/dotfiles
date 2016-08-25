ef6ce5f6f142f9636ea1c3d8f82d3beeae6f7ef8
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2016-08-25 13:10:21 +0200

    Added fonts to setup 🐺

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Gitconfig & Zshrc
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+
+# Fonts
+if [ -d "$HOME/Library/Fonts" ]; then
+  sudo cp $HOME/dotfiles/fonts/* $HOME/Library/Fonts
+fi
+
+if [ -d "/usr/local/share/fonts" ]; then
+  sudo cp $HOME/dotfiles/fonts/* /usr/share/fonts
+fi
